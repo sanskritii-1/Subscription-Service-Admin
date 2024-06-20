26f0879611ea47ad20581eeff19a3c4181919194
Pranjal-sh10 <9921103226@mail.jiit.ac.in>
2024-06-21 04:42:16 +0530

    CurrentPlan+PaymentHistory done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+<<<<<<< refs/remotes/origin/CurrentPlan
 import CurrentPlansPage from './pages/CurrentPlansPage';
 import Home from './pages/Home';
+=======
+import Home from './pages/Home';
+import InfoPage from './pages/InfoPage';
+import CurrentPlanPage from './pages/CurrentPlan';
+>>>>>>> local
 
 const router = createBrowserRouter([
   {
     path: '/',
     children: [
       { path: '', element: <Home /> },
+<<<<<<< refs/remotes/origin/CurrentPlan
       { path: 'Plans-User' ,element: <CurrentPlansPage/>},
+=======
+      { path: 'PaymentInfo' ,element: <InfoPage/>},
+      { path: 'CurrentPlan', element: <CurrentPlanPage/> },
+>>>>>>> local
     ],
   },
 ]);
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
 import Home from '../pages/Home'
+<<<<<<< refs/remotes/origin/CurrentPlan
 import CurrentPlansPage from '../pages/CurrentPlansPage';
+=======
+>>>>>>> local
 
 const router = createBrowserRouter([
     {
