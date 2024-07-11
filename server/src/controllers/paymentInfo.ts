da4d2da5ac08b3711a8517264a59a3cead00ba7a
sanskritii-1 <nsanskriti03@gmail.com>
2024-07-11 10:20:52 +0530

    added pagination, sorting transaction table

--- server/src/controllers/paymentInfo.ts
+++ server/src/controllers/paymentInfo.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import User, { IUser } from '../models/user';
 import Plan, { IPlan } from '../models/plan';
 import Subscription, { ISubscription } from '../models/subscription';
-import {success} from "../utils/response";
+import { success } from "../utils/response";
 import Transaction, { ITransaction } from '../models/transaction';
 import { CustomError } from '../middlewares/error';
 
@@ -18,23 +18,23 @@ export const getCurrentPlans = async (req: Request, res: Response, next: NextFun
           planId: { $first: '$planId' },
         },
       },
-    ]); 
-      
-      console.log(subscriptions);
+    ]);
+
+    console.log(subscriptions);
 
     const paymentHistory = await Promise.all(subscriptions.map(async (subscription) => {
       const user = await User.findById<IUser>(subscription._id);
       const plan = await Plan.findById<IPlan>(subscription.planId);
       return {
         id: subscription.subscriptionId,
-        userName: user?.name,    
-        userEmail: user?.email,   
-        planName: plan?.name,     
+        userName: user?.name,
+        userEmail: user?.email,
+        planName: plan?.name,
         startDate: subscription.startDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }),
       };
     })
-  );
-    return res.status(200).json(success(200,{paymentHistory}));
+    );
+    return res.status(200).json(success(200, { paymentHistory }));
   } catch (error) {
     console.error('Error fetching payment history:', error);
     next(error);
@@ -42,32 +42,59 @@ export const getCurrentPlans = async (req: Request, res: Response, next: NextFun
 };
 
 
-export const getTransactions = async (req: Request, res: Response, next: NextFunction): Promise<Response|void> => {
+export const getTransactions = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
   try {
-    const records = await Transaction.find<ITransaction>();
-    if(!records){
-      const err: CustomError = new Error("No transaction records found");
-      err.status = 404;
-      return next(err);
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 2;
+    const skip = (page - 1) * limit;
+
+    const keyword = req.query.keyword;
+    const planName = req.query.planName as string;
+    const updatedAt = req.query.updatedAt as string;
+    const isAsc = req.query.isAsc === "true";
+
+    const conditions: any = {};
+
+    if (keyword) {
+      const users = await User.find({ name: { $regex: keyword, $options: 'i' } }, '_id').lean() as Array<IUser & { _id: string }>;
+      const userIds = users.map(user => user._id);
+      conditions.userId = { $in: userIds };
     }
+
+
+    const sortOrder = isAsc ? 1 : -1;
+    const records = await Transaction.find<ITransaction>(conditions).sort({ updatedAt: sortOrder }).skip(skip).limit(limit);
+
+    // if (!records) {
+    //   const err: CustomError = new Error("No transaction records found");
+    //   err.status = 404;
+    //   return next(err);
+    // }
 
     const paymentHistory = await Promise.all(records.map(async (subscription) => {
       const user = await User.findById<IUser>(subscription.userId);
       const plan = await Plan.findById<IPlan>(subscription.planId);
       return {
         id: subscription._id,
-        userName: user?.name,    
-        userEmail: user?.email,   
-        planName: plan?.name, 
-        amount: subscription.amount,    
+        userName: user?.name,
+        userEmail: user?.email,
+        planName: plan?.name,
+        amount: subscription.amount,
         date: subscription.updatedAt,
         paymentMethod: subscription.paymentMethod,
         status: subscription.status,
       };
     }))
 
-    return res.status(200).json(success(200,{paymentHistory}));
-    
+    const totalCount = await Transaction.countDocuments(conditions);
+
+    return res.status(200).json(success(200, { paymentHistory, pagination:{
+      total: totalCount,
+      page,
+      limit,
+      totalPages: Math.ceil(totalCount/limit),
+    } }));
+
   } catch (err) {
     next(err);
   }
